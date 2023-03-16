7b29cb636817a9e23cd3a68d90bded54be4e5d3a
ajilibrahim20 <ajil.ibrahim@batonsystems.com>
2023-03-16 16:49:44 +0530

    upgraded

--- product-webapp/webapp/src/components/landing/LandingPage.js
+++ product-webapp/webapp/src/components/landing/LandingPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import HeroSection from "./HeroSection";
 
 import ProductFeatures from "./ProductLanding";
 const LandingPage = () => {
   return (
     <>
 
-      <HeroSection />
 
 <ProductFeatures imageUrl={"https://picsum.photos/1200/700"} title="Product Title" description="Product Description goes here"/>
 
