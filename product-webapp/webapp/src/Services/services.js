5c32a12c9e2904b9be872870b5da4daa2e58243e
ajilibrahim20 <ajil.ibrahim@batonsystems.com>
2023-03-27 16:41:51 +0530

    modified

--- product-webapp/webapp/src/Services/services.js
+++ product-webapp/webapp/src/Services/services.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
 export const register=(data)=>{
-    axios.post("http://localhost:8086/register",data)
+    const datas={
+        userName:`${data.firstName} ${data.lastName}`,
+        contactNumber:`${data.phoneNumber}`,
+        emailId:`${data.email}`,
+        password:`${data.password}`,
+        address:`${data.address1} ${data.address2}`
+    }
+    console.log(datas);
+    axios.post("http://localhost:8086/register",datas)
     .then((res)=>{
+
+
         if(res.status===200){
-        alert("registeration suceessfull")
+        alert("registration successful")
         }
     });
 }
