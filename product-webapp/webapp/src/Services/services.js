886e0c908b97c4f289cb3d977c2c960f1158d637
ajilibrahim20 <ajil.ibrahim@batonsystems.com>
2023-03-27 11:19:06 +0530

    validated signup

--- product-webapp/webapp/src/Services/services.js
+++ product-webapp/webapp/src/Services/services.js
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+export const register=(data)=>{
+    axios.post("http://localhost:8086/register",data)
+    .then((res)=>{
+        if(res.status===200){
+        alert("registeration suceessfull")
+        }
+    });
+}
