e59cff8e998e063fc22c7c7892e812035fc4f0cf
Aksh Sood <aksh.sood@batonsystems.com>
2023-03-28 17:00:56 +0530

    cross origin up

--- wallet-service/src/main/java/com/stackroute/walletservice/controller/WalletController.java
+++ wallet-service/src/main/java/com/stackroute/walletservice/controller/WalletController.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin()
 @RestController
 @RequestMapping("/wallet")
 @Validated
