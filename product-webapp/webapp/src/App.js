55bf935a1e7e83d48a5df30e21c1c4cd575d02b2
Yusuf Siddiqui <Yusuf.Siddiqui@in.niit.com>
2023-03-30 15:35:47 +0530

    Add Hashrouter

--- product-webapp/webapp/src/App.js
+++ product-webapp/webapp/src/App.js
@@ -6,43 +6,35 @@ import LandingPage from "./components/landing/LandingPage";
 import UserProfile from "./components/userProfile/UserProfile";
 import SendMoney from "./components/sendMoney/SendMoney";
 import AddMoney from "./components/addMoney/AddMoney";
-import WithdrawMoney from "./components/widthdrawMoney/WithdrawMoney";
 import Dashboard from "./components/dashboard/Dashboard";
 import Transaction from "./components/history/Transaction";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { Route, HashRouter as Router } from "react-router-dom";
 import PrivateRoute from "./components/privateRoute/PrivateRoute";
 import WalletRoute from "./components/walletRoute/WalletRoute";
 
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<LandingPage />} />
-      <Route exact path="/signIn" element={<SignIn />} />
-      <Route exact path="/signUp" element={<SignUp />} />
-
-
-      <Route exact path="/dashboard" element={<PrivateRoute> <Dashboard /> </PrivateRoute>} />
-      <Route exact path="/updateUser" element={<PrivateRoute> <UpdateUser /> </PrivateRoute>} />
-      <Route exact path="/userProfile" element={<PrivateRoute> <UserProfile /> </PrivateRoute>} />
-
-
-      <Route exact path="/sendMoney" element={<WalletRoute> <SendMoney /> </WalletRoute>} />
-      <Route exact path="/addMoney" element={<WalletRoute> <AddMoney /> </WalletRoute>} />
-      <Route exact path="/transaction" element={<WalletRoute> <Transaction /> </WalletRoute>} />
-    </Route>
-  )
-);
-
 function App() {
   return (
-    <>
-      <div className="App">
-        <RouterProvider router={router} />
-      </div>
-    </>
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/signIn" exact component={SignIn} />
+        <Route path="/signUp" exact component={SignUp} />
+
+        <PrivateRoute path="/dashboard" exact component={Dashboard} />
+        <PrivateRoute path="/updateUser" exact component={UpdateUser} />
+        <PrivateRoute path="/userProfile" exact component={UserProfile} />
+
+        <WalletRoute path="/sendMoney" exact component={SendMoney} />
+        <WalletRoute path="/addMoney" exact component={AddMoney} />
+        <WalletRoute path="/transaction" exact component={Transaction} />
+      </Router>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
