a5273ce9679e7700f37e91399d640f2d3958489d
Yusuf Siddiqui <Yusuf.Siddiqui@in.niit.com>
2023-03-30 15:43:28 +0530

    Change router

--- product-webapp/webapp/src/App.js
+++ product-webapp/webapp/src/App.js
@@ -8,14 +8,15 @@ import SendMoney from "./components/sendMoney/SendMoney";
 import AddMoney from "./components/addMoney/AddMoney";
 import Dashboard from "./components/dashboard/Dashboard";
 import Transaction from "./components/history/Transaction";
-import { Route, HashRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import PrivateRoute from "./components/privateRoute/PrivateRoute";
 import WalletRoute from "./components/walletRoute/WalletRoute";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
+      <Routes>
         <Route path="/" exact component={LandingPage} />
         <Route path="/signIn" exact component={SignIn} />
         <Route path="/signUp" exact component={SignUp} />
@@ -27,7 +28,8 @@ function App() {
         <WalletRoute path="/sendMoney" exact component={SendMoney} />
         <WalletRoute path="/addMoney" exact component={AddMoney} />
         <WalletRoute path="/transaction" exact component={Transaction} />
-      </Router>
+      </Routes>
+      </HashRouter>
     </div>
   );
 }
