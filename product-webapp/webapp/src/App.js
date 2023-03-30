057e9722250523e5b539adfbf4f95837771cc73e
Yusuf Siddiqui <Yusuf.Siddiqui@in.niit.com>
2023-03-30 17:37:15 +0530

    Resolved conflicts

--- product-webapp/webapp/src/App.js
+++ product-webapp/webapp/src/App.js
@@ -1,41 +1,3 @@
-// import "./App.css";
-// import SignIn from "./components/signIn/SignIn";
-// import SignUp from "./components/signUp/SignUp";
-// import UpdateUser from "./components/UpdateUser/UpdateUser";
-// import LandingPage from "./components/landing/LandingPage";
-// import UserProfile from "./components/userProfile/UserProfile";
-// import SendMoney from "./components/sendMoney/SendMoney";
-// import AddMoney from "./components/addMoney/AddMoney";
-// import Dashboard from "./components/dashboard/Dashboard";
-// import Transaction from "./components/history/Transaction";
-// import { Route, Routes, HashRouter } from "react-router-dom";
-// import PrivateRoute from "./components/privateRoute/PrivateRoute";
-// import WalletRoute from "./components/walletRoute/WalletRoute";
-
-// function App() {
-//   return (
-//     <>
-//       <HashRouter>
-//       <Routes>
-//         <Route path="/" exact component={LandingPage} />
-//         <Route path="/signIn" exact component={SignIn} />
-//         <Route path="/signUp" exact component={SignUp} />
-
-//         <PrivateRoute path="/dashboard" exact component={Dashboard} />
-//         <PrivateRoute path="/updateUser" exact component={UpdateUser} />
-//         <PrivateRoute path="/userProfile" exact component={UserProfile} />
-
-//         <WalletRoute path="/sendMoney" exact component={SendMoney} />
-//         <WalletRoute path="/addMoney" exact component={AddMoney} />
-//         <WalletRoute path="/transaction" exact component={Transaction} />
-//       </Routes>
-//       </HashRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import SignIn from "./components/signIn/SignIn";
 import SignUp from "./components/signUp/SignUp";
@@ -46,13 +8,17 @@ import SendMoney from "./components/sendMoney/SendMoney";
 import AddMoney from "./components/addMoney/AddMoney";
 import Dashboard from "./components/dashboard/Dashboard";
 import Transaction from "./components/history/Transaction";
-import { createBrowserRouter, createRoutesFromElements, HashRouter, Route, RouterProvider } from "react-router-dom";
+import {  HashRouter, Route,Routes } from "react-router-dom";
 import PrivateRoute from "./components/privateRoute/PrivateRoute";
 import WalletRoute from "./components/walletRoute/WalletRoute";
-const router = createBrowserRouter(
-  createRoutesFromElements(
+
+function App() {
+  return (
+    <>
+      <div className="App">
+     
     <HashRouter>
-    <Route>
+      <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route exact path="/signIn" element={<SignIn />} />
       <Route exact path="/signUp" element={<SignUp />} />
@@ -62,15 +28,10 @@ const router = createBrowserRouter(
       <Route exact path="/sendMoney" element={<WalletRoute> <SendMoney /> </WalletRoute>} />
       <Route exact path="/addMoney" element={<WalletRoute> <AddMoney /> </WalletRoute>} />
       <Route exact path="/transaction" element={<WalletRoute> <Transaction /> </WalletRoute>} />
-    </Route>
+
+      </Routes>
     </HashRouter>
-  )
-);
-function App() {
-  return (
-    <>
-      <div className="App">
-        <RouterProvider router={router} />
+    
       </div>
     </>
   );
